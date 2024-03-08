cdde7f94365b4186b8c5be92e0c9014f0b264788
LindaPermata <permatalinda531@gmail.com>
2024-03-08 16:31:41 +0700

    Add project files.

---
+++ bublesort/bublesort.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int arr[20]; //deklarasi variabel global array a dengan ukuran 20
+int n;  //deklarasi variabel global n untuk menyimpan banyaknya elemen array
